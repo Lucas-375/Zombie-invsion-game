a7a7865d7d3c5af4a58ff9def9d68a3927dc6136
Lucas-375 <lucas.belizario375@gmail.com>
2025-06-09 14:44:42 +1000

    Update game assets and background images; replace player rectangle with soldier image

--- games/game.py
+++ games/game.py
@@ -11,20 +11,25 @@ class Game:
         self.vel = 5
 
         # Colors
-        self.BG = (50, 50, 50)
         self.RED = (255, 0, 0)
         self.YELLOW = (255, 255, 0)
         self.GREEN = (0, 255, 0)
         self.BLUE = (0, 0, 255)
 
+        # Background
+        self.BG = pygame.image.load('images/background/combined_background.png').convert()
+        self.BG = pygame.transform.scale(self.BG, (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+       
         # Font
         self.font = pygame.font.Font('fonts/Daydream.ttf', 24)
 
         # Rectangles
-        self.rect_1 = pygame.Rect(200, 100, 50, 50)
         self.obstacle = pygame.Rect(300, 100, 50, 50)
 
         # Images
+        self.player_rect = pygame.Rect(155, 260, 50, 50)
+        self.soldier = pygame.image.load('images/soldier.png').convert_alpha()
+        self.soldier = pygame.transform.scale(self.soldier, (50, 50))
 
         self.refresh_rect = pygame.Rect(400, 345, 50, 50)
         self.refresh = pygame.image.load('images/refresh_black.png').convert_alpha()
@@ -47,9 +52,6 @@ class Game:
         
         self.max_bullets = 3
         self.current_bullets = self.max_bullets
-
-        self.bullet_gif = pygame.image.load('images/bullet.gif').convert_alpha()
-        self.bullet_gif = pygame.transform.scale(self.bullet_gif, (self.bullet_width, self.bullet_height))
 
         self.frames = []
         for i in range(1, 7):
@@ -84,23 +86,27 @@ class Game:
             self.update_animation()
 
     def moving_keys(self, key):
-        if key[pygame.K_d] and self.rect_1.right < self.SCREEN_WIDTH:
-            self.rect_1.move_ip(self.vel, 0)
-        if key[pygame.K_a] and self.rect_1.left > 0:
-            self.rect_1.move_ip(-self.vel, 0)
-        if key[pygame.K_w] and self.rect_1.top > 0:
-            self.rect_1.move_ip(0, -self.vel)
-        if key[pygame.K_s] and self.rect_1.bottom < self.SCREEN_HEIGHT:
-            self.rect_1.move_ip(0, self.vel)
+        # if key[pygame.K_d] and self.player_rect.right < self.SCREEN_WIDTH:
+        #     self.player_rect.move_ip(self.vel, 0)
+        # if key[pygame.K_a] and self.player_rect.left > 0:
+        #     self.player_rect.move_ip(-self.vel, 0)
+        # if key[pygame.K_w] and self.player_rect.bottom > 310:
+        #     self.player_rect.move_ip(0, -self.vel)
+        # if key[pygame.K_s] and self.player_rect.bottom < self.SCREEN_HEIGHT:
+        #     self.player_rect.move_ip(0, self.vel)
 
+        if key[pygame.K_w] and self.player_rect.bottom > 310:
+            self.player_rect.move_ip(self.vel, -self.vel)
+        if key[pygame.K_s] and self.player_rect.bottom < self.SCREEN_HEIGHT:
+            self.player_rect.move_ip(-self.vel, self.vel)
     def shooting(self):
         current_time = pygame.time.get_ticks()
         if (
             self.current_bullets > 0
             and (current_time - self.last_shot_time > 350 or self.last_shot_time == 0)
         ):
-            bullet_x = self.rect_1.x + self.rect_1.width
-            bullet_y = self.rect_1.y + self.rect_1.height / 2 - 5
+            bullet_x = self.player_rect.x + self.player_rect.width
+            bullet_y = self.player_rect.y + self.player_rect.height / 2 - 11.5
 
             if len(self.bullets) < self.max_bullets:
                 bullet = self.Bullet(
@@ -128,11 +134,12 @@ class Game:
         while run:
             self.clock.tick(60)
             current_time = pygame.time.get_ticks()
-            self.screen.fill(self.BG)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+
+            self.screen.blit(self.BG, (0, 0))
 
             key = pygame.key.get_pressed()
             self.moving_keys(key)
@@ -170,7 +177,7 @@ class Game:
             ]
 
             # Draw player rect
-            pygame.draw.rect(self.screen, self.BLUE, self.rect_1)
+            self.screen.blit(self.soldier, self.player_rect)
 
             # Draw text
             font_render = self.font.render(
@@ -191,3 +198,5 @@ class Game:
 if __name__ == "__main__":
     game = Game()
     game.run_game()
+
+
