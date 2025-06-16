57f828b40109532a7e27b541de98e8ec1e4ccd80
Lucas-375 <lucas.belizario375@gmail.com>
2025-06-17 09:44:22 +1000

    adding docstrings

--- games/ultilities/bullet_class.py
+++ games/ultilities/bullet_class.py
@@ -11,14 +11,17 @@ class Bullet:
             self.screen = screen
 
         def move(self):
+            """Move the bullet."""
             self.rect.x += self.speed
 
         def update_animation(self):
+            """Update the bullet's frame gif animation."""
             now = pygame.time.get_ticks()
             if now - self.last_update > self.frame_delay:
                 self.last_update = now
                 self.current_frame = (self.current_frame + 1) % len(self.frames)
 
         def draw(self):
+            """Draw the bullet."""
             self.screen.blit(self.frames[self.current_frame], self.rect.topleft)
             self.update_animation()
--- games/ultilities/zombies_class.py
+++ games/ultilities/zombies_class.py
@@ -19,6 +19,7 @@ class Zombie(HPSystem):
         super().__init__(max_hp=100, screen=screen, entity_rect=self.rect)
 
     def move(self):
+        """Move a zombie to the left"""
         if self.rect.x > 0:
             self.rect.x -= self.vel 
         else:
@@ -26,87 +27,18 @@ class Zombie(HPSystem):
             self.rect.y = randint(230, self.screen.get_height() - ZOMBIE_HEIGHT) # Draw the HP bar above the zombie
         
     def update_animation(self):
+        """Update the zombie's frame gif animation."""
         now = pygame.time.get_ticks()
         if now - self.last_frame_update > self.frame_rate:
             self.current_frame = (self.current_frame + 1) % len(self.frames)
             self.last_frame_update = now
 
     def draw(self):
+        """Draw the zombie"""
         self.update_animation()
         self.screen.blit(self.frames[self.current_frame], self.rect.topleft)    
         super().draw_hp_bar()  # Draw the HP bar above the zombie
 
     def collides_with(self, other_rect):
+        """Check if the zombie collided with another rect."""
         return self.rect.colliderect(other_rect)
-
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.SCREEN_WIDTH = 600
-        self.SCREEN_HEIGHT = 400
-        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.vel = 2.3
-        pygame.display.set_caption('Zombies Coming')
-
-        # Colors
-        self.BG = (50, 50, 50)
-        self.GREEN = (0, 255, 0)
-
-        # Zombie gif frames
-        self.frames = []
-        for i in range(1, 17):
-            frame = pygame.image.load(f'images/zombie1_gif/zombie1_{i}.png').convert_alpha()
-            frame = pygame.transform.scale(frame, (ZOMBIE_WIDTH, ZOMBIE_HEIGHT))
-            self.frames.append(frame)
-
-        # Zombies list
-        self.zombies = []
-        for _ in range(randint(5, 8)):
-            self.add_zombie()
-
-        # Player
-        self.player_rect = pygame.Rect(300, 150, 50, 50)
-
-    def add_zombie(self):
-        x = randint(450, 550)
-        y = randint(230, self.SCREEN_HEIGHT - ZOMBIE_HEIGHT)
-        zombie = Zombie(x, y, self.frames, self.vel)
-        self.zombies.append(zombie)
-
-    def remove_collided_zombie(self, rect):
-        for zombie in self.zombies:
-            if zombie.collides_with(rect):
-                self.zombies.remove(zombie)
-                return True
-        return False
-
-    def run_game(self):
-        running = True
-        while running:
-            self.clock.tick(60)
-            self.screen.fill(self.BG)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-            # Move and draw zombies
-            for zombie in self.zombies:
-                zombie.move()
-                zombie.draw(self.screen)
-
-            # Draw the player rectangle
-            pygame.draw.rect(self.screen, (255, 0, 0), self.player_rect)
-
-            # Remove collided zombies and add new ones
-            if self.remove_collided_zombie(self.player_rect):
-                self.add_zombie()
-
-            pygame.display.flip()
-
-        pygame.quit()
-
-if __name__ == "__main__":
-    game = Game()
-    game.run_game()
